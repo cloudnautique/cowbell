f345cd376b44c3a5b5834af95511a3fa2be66c55
Bill Maxwell <bill@rancher.com>
2015-11-11 15:38:07 -0700

    fixed out of index error

--- server/handlers.go
+++ server/handlers.go
@@ -14,13 +14,15 @@ func ScaleUp(w http.ResponseWriter, r *http.Request) {
 	variables := mux.Vars(r)
 	response := respond("message", 404)
 
-	logrus.Infof("Request to scale service: %s", variables["serviceName"])
-	if checkServiceToken(variables["serviceName"], r.URL.Query()["token"][0]) {
-		logrus.Infof("Scaling service...%s", variables["serviceName"])
+	if tokenList, ok := r.URL.Query()["token"]; ok {
+		logrus.Infof("Request to scale service: %s", variables["serviceName"])
+		if checkServiceToken(variables["serviceName"], tokenList[0]) {
+			logrus.Infof("Scaling service...%s", variables["serviceName"])
 
-		//quickly grab the token and respond. This action can take a while.
-		go context.ScaleServiceUp(variables["serviceName"])
-		response = respond("message", 202)
+			//Async send a request and respond. This action can take a while.
+			go context.ScaleServiceUp(variables["serviceName"])
+			response = respond("message", 202)
+		}
 	}
 
 	output, _ := json.Marshal(response)
